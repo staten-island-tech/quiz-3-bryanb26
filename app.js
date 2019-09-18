a260e92146ee166564e2f13d6b5d51fa8100fc77
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-09-18 11:40:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function tipCalc(bills){
+/* function tipCalc(bills){
     let tip;
     if (bills < 50) {
         percentage = .2;
@@ -22,4 +22,30 @@ function isGreater(num){
 
 let copy = finalCosts.filter(isGreater);
 
-console.log(copy);
+console.log(copy); */
+
+const Rob = {
+    fullName: 'Robert Grande',
+    mass: 78,
+    height: 1.69,
+    bmi: function(){
+        return this.mass / (this.height * this.height);
+    }
+};
+
+const Dejon = {
+    fullName: 'Dejon Kurti',
+    mass: 110,
+    height: 1.95,
+    bmi: function(){
+        return this.mass / (this.height * this.height);
+    }
+};
+
+if(Rob.bmi()>Dejon.bmi()){
+    console.log(`${Rob.fullName} has a BMI of ${Rob.bmi()} which is greater than Dejon's!`);
+}else if(Dejon.bmi()>Rob.bmi()){
+    console.log(`${Dejon.fullName} has a BMI of ${Dejon.bmi()} which is greater than Rob's!`);
+}else{
+    console.log('Sadly, these young men have the same BMI...')
+};
